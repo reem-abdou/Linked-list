d06426689174f9dcb0f08a01a91b88278f1aa8e9
reem-abdou <146747432+reem-abdou@users.noreply.github.com>
2024-09-27 09:11:54 +0300

    Update Creat_LinkedList.cpp

--- Creat_LinkedList.cpp
+++ Creat_LinkedList.cpp
@@ -2,7 +2,7 @@
 
 using namespace std; 
 
-// Creat a node which contains data value and a pointer and make it public to can acces
+// Create a node which contains data value and a pointer and make it public to can access
 class Node { 
 public: 
     int data;
@@ -11,7 +11,7 @@ public:
 
 class Linkedlist { 
 public: 
-    Node* head; // Creat a pointer of type Node to point at the first item in the list 
+    Node* head; // Create a pointer of type Node to point at the first item in the list 
 
     Linkedlist() { 
         head = NULL; // The list is initially empty
@@ -25,26 +25,24 @@ public:
     void insertbegin(int newitem) { 
         Node* newnode = new Node; // Create a new node
         newnode->data = newitem; // Set the data for the new node
-        newnode->next = head; // Point the new node's next to head (to points to the previous item)
-        head = newnode; // Make haed points to the first item 
+        newnode->next = head; // Point the new node's next to head (to point to the previous item)
+        head = newnode; // Make head point to the first item 
     } 
 
     void insertbeforeitem(int newvalue, int item) {
         if (empty()) {
             cout << "The list is empty";
-            return;
         }
         else if (head->data == item) { // Insert at the beginning
             Node* newnode = new Node(); // Create a new node
             newnode->data = newvalue;  // Set the data for the new node
             newnode->next = head;
             head = newnode;
-            return;
         }
         else if (isfound(item)) {  // Check if the item exists in the linked list
             Node* newnode = new Node(); // Create a new node
             newnode->data = newvalue;  // Set the data for the new node
-            Node* prev = head;    // Creat a pointer points to the first item
+            Node* prev = head;    // Create a pointer points to the first item
             // Make previous pointer points to the node before the 'item' node
             while (prev != NULL && prev->next->data != item) {
                 prev = prev->next;  // Move to the next node
@@ -65,7 +63,7 @@ public:
             head = newnode; // Make head points to the first item
         }
         else {
-            Node* temp = head; // Creat a template pointer points to the first item
+            Node* temp = head; // Create a template pointer points to the first item
             while (temp->next != NULL) { // Make temp pointer points to the last item
                 temp = temp->next;
             }
@@ -73,9 +71,9 @@ public:
         }
     }
 
-    // Creat method to Display items in the list
+    // Create method to Display items in the list
     void Display() { 
-        Node* temp = head; // Creat a template pointer points to the first item
+        Node* temp = head; // Create a template pointer points to the first item
         while (temp != NULL) { // Loop until the end of the list
             cout << temp->data << " "; 
             temp = temp->next; // Move to the next node
@@ -83,9 +81,9 @@ public:
         cout << '\n';
     } 
 
-    // Creat method to check if a value is found or not
+    // Create method to check if a value is found or not
     bool isfound(int x) { 
-        Node* temp = head; // Creat a template pointer points to the first item
+        Node* temp = head; // Create a template pointer points to the first item
         bool F = false; // Flag to indicate if the item is found and set it = false by default
         while (temp != NULL) { // Loop until the end of the list
             if (temp->data == x) { 
@@ -100,7 +98,7 @@ public:
     // Method to delete an 'item' from the list
     void Delete(int item) {
         if (empty()) {
-            cout << "The List is empty , no items to delete \n";
+            cout << "The List is empty, no items to delete \n";
             return;
         }
         else if (!isfound(item)) {
@@ -117,22 +115,22 @@ public:
             Node* prev = NULL;
             Node* delptr = head;
             while (delptr->data != item) {
-                prev = delptr;          // Make prev points to the previouse node 
+                prev = delptr;          // Make prev points to the previous node 
                 delptr = delptr->next;  // Make delptr points to the node that will be deleted
             }
-            prev->next = delptr->next;  // Make the previouse node points to the following node 
+            prev->next = delptr->next;  // Make the previous node points to the following node 
             delete delptr;   // Delete the Node
         }
     }
     
-    // Method deallocate the memory to prevent memory leak
+    // Method to deallocate the memory to prevent memory leak
     void finish(){
-        while(head!=NULL){ // Loop until the end of the list
-            Node* temp = head; // Creat a template pointer points to the first item
+        while(head != NULL) { // Loop until the end of the list
+            Node* temp = head; // Create a template pointer points to the first item
             head = head->next; // Move to the next node
             delete temp;  
         } 
-	}
+    }
 }; 
 
 int main() { 
@@ -150,7 +148,7 @@ int main() {
     cout << "Please enter the items, separating each with a space: \n";
     for (int i = 0; i < num; i++) {  
         cin >> newitem; 
-        lst.insertbegin(newitem); 
+        lst.append(newitem); 
     } 
 
     cout << "Your list is: \n";
@@ -158,48 +156,59 @@ int main() {
    
     cout << "Enter a value that you want to append: \n";
     cin >> newitem;
-    lst.append(newitem);
+    lst.append(newitem); 
+   
+    cout << "The New item is added successfully \n"
+         << "Your New list is: \n";
+    lst.Display();
+   
+    cout << "Insert an item at the beginning of the list: \n";
+    cin >> newitem;
+    lst.insertbegin(newitem); 
    
     cout << "The New item is added successfully \n"
          << "Your New list is: \n";
     lst.Display();
    
     int place;
-    cout << "Enter a value that you want to add and its place: \n";
-    cout << "New value: ";
+    cout << "Add new value before a specific item \n";
+    cout << "Enter the new value: \n";
     cin >> newitem;
-    cout << "Its place: ";
+    cout << "Enter the item you want to add before: \n";
     cin >> place; 
     lst.insertbeforeitem(newitem, place);
-
-    cout << "The New item is added successfully \n"
-         << "Your New list is: \n";
-    lst.Display();
+ 
+    if(lst.isfound(newitem)) {
+        cout << "The New item is added successfully \n"
+             << "Your New list is: \n";
+        lst.Display();
+    }
    
     int item;
     cout << "Enter a value that you want to delete: \n";
     cin >> item;
     lst.Delete(item);
    
-    cout << "The item is deleted successfully \n"
-         << "Your New list is: \n";
-    lst.Display();
+    if (!lst.isfound(item)) {
+        cout << "The item has been deleted successfully \n"
+             << "Your New list is: \n";
+        lst.Display();
+    }
    
     int x;
-    cout << "Enter an value to check: \n";
+    cout << "Enter a value to check: \n";
     cin >> x; 
     if (lst.isfound(x)) {
         cout << "It is found \n";
     }
     else {
-        cout << "I is not found \n";
+        cout << "It is not found \n";
     }
     
     lst.finish();
     if (lst.empty()) {
-        cout << "The proram is finished and the list is empty! \n";
+        cout << "The program is finished and the list is empty! \n";
     }
     
-   
     return 0; 
 }
